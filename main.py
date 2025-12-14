6639be4d31c28e6db857959300c750fd87e85b18
ValkaDD <ValkaDaChad@gmail.com>
2025-12-14 19:24:52 +0100

    C: Finalization

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from stats import word_count, get_book_text, listofdic
+import sys
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     print("============ BOOKBOT ============\n")
-    print("Analyzing book found at books/frankenstein.txt...\n")
+    print(f"Analyzing book found at {book_path}...\n")
     print("----------- Word Count ----------\n")
-    sentence = get_book_text("books/frankenstein.txt")
+    sentence = get_book_text(book_path)
     print(f"Found {word_count(sentence)} total words\n")
     print("--------- Character Count -------\n")
     CharactersList = listofdic(sentence)
